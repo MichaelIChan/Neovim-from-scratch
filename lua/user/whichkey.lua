7fbba135efcf5349194134a82f9b62f183670fc6
chengzh <loveandloss@hotmail.com>
2022-06-18 22:46:38 +0800

    add plugin diffview

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -107,11 +107,13 @@ local mappings = {
 
   g = {
     name = "Git",
+    f = { "<cmd>DiffviewFileHistory<CR>", "File History" },
     g = { "<cmd>lua _LAZYGIT_TOGGLE()<CR>", "Lazygit" },
     j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
     k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
     l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
     p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
+    P = { "<cmd>DiffviewOpen<CR>", "Diff Project" },
     r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
     R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
     s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
